4f54b1b7a243f0086e8a4b4186c749d3a95ce43c
Chaithanya <your-email@example.com>
2026-06-16 18:19:26 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import products from './pages/products'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
